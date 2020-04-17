924a89704a90dbf3918df578c89b49ee2bd2eb7e
splincode <omaxphp@yandex.ru>
2020-04-17 16:36:48 +0300

    only dependencies

--- tools/publisher.ts
+++ tools/publisher.ts
@@ -13,26 +13,23 @@ console.log('NPM_TOKEN', !!token);
 
 const pkg: PlainObject = JSON.parse(readFileSync('package.json').toString());
 
-exec(
-    `npm show ${pkg.name} version`,
-    (error: ExecException | null, stdout: string, stderr: string): void => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            console.error(stderr);
-        }
-
-        const oldVersion: string = stdout;
-        const version: string[] = stdout.split('.');
-        const major: number = parseInt(version[0]);
-        const minor: number = parseInt(version[1]) + 1;
-        const newVersion: string = `${major}.${minor}.0`;
-
-        pkg.version = newVersion;
-
-        console.log(`\nPrev "${pkg.name}" version: ${oldVersion}`);
-        console.log(`New "${pkg.name}" version: ${newVersion}\n`);
-
-        // eslint-disable-next-line no-magic-numbers
-        writeFileSync('package.json', JSON.stringify(pkg, null, 4));
+exec(`npm show ${pkg.name} version`, (error: ExecException | null, stdout: string, stderr: string): void => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        console.error(stderr);
     }
-);
+
+    const oldVersion: string = stdout;
+    const version: string[] = stdout.split('.');
+    const major: number = parseInt(version[0]);
+    const minor: number = parseInt(version[1]) + 1;
+    const newVersion: string = `${major}.${minor}.0`;
+
+    pkg.version = newVersion;
+
+    console.log(`\nPrev "${pkg.name}" version: ${oldVersion}`);
+    console.log(`New "${pkg.name}" version: ${newVersion}\n`);
+
+    // eslint-disable-next-line no-magic-numbers
+    writeFileSync('package.json', JSON.stringify(pkg, null, 4));
+});
